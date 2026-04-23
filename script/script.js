7634f410c9bdfb8ca66b1a4c88e2cbd9e131c3b6
Wilson <109673935+wilsongneto@users.noreply.github.com>
2026-04-23 17:21:47 -0300

    js

--- script/script.js
+++ script/script.js
@@ -14,4 +14,30 @@ function salvarCarros(event) {
     let lista = document.getElementById('listarCarros');
 
     let card = document.createElement('div');
+
+    card.style.border = "1px solid #ccc";
+    card.style.padding = "10px";
+    card.style.marginBottom = "10px";
+
+    card.innerHTML = '
+    <h3>${titulo}</h3>
+    <p><strong>Preço:</strong> R$ ${preco}</p>
+    <p><strong>Marca:</strong> R$ ${marca}</p>
+    <p><strong>Modelo:</strong> R$ ${modelo}</p>
+    <p><strong>Câmbio:</strong> R$ ${cambio}</p>
+    ';
+
+    lista.appendChild(card);
+
+    let carro = {
+        titulo,
+        preco,
+        marca,
+        modelo, 
+        cambio
+    };
+
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
+    carros.push(carro);
+    localStorage.setItem("carros", JSON.stringify(carros));
 }
